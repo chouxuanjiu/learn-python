eb201a09f674068717a54c2713eb4bba96bb137b
chouxuanjiu <chouxuanjiu@gmail.com>
2019-08-29 13:06:10 +0800

    test commit

---
+++ D001-001.py
@@ -0,0 +1,11 @@
+"""
+Python自学笔记
+Day1 19/8/19 初识Python
+"""
+#使用print和input
+
+print('100+300=',100+300)
+
+usrname = input('plz enter ur name:')
+print('Hello',usrname, sep=', ', end='!\n')
+print( 'Welcome 2 the Huashan Hotel!')
